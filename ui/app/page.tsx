5debc3d165becad50c0368ffa341a9dd6eb50e8c
denhampreen <denhampreen@gmail.com>
2025-08-16 18:51:30 +0100

    fix: ui build

--- ui/app/page.tsx
+++ ui/app/page.tsx
@@ -4,7 +4,7 @@ import { notFound } from 'next/navigation';
 
 export default async function HomePage() {
     const data = await fetchBlacklistData({ pageUsdt: 1, pageUsdc: 1, pageSize: 20, sortUsdt: 'desc', sortUsdc: 'desc' });
-    return <PageContent selectedTab="usdt" data={data} />;
+    return <PageContent selectedTab="usdt" data={data} currentPage={1} sort={'desc'} />;
 }
 
 
